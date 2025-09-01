33f36bf3f28d69b00e5e27d335010a5d4283ab14
ariful599 <ariful.islam.siam599@gmail.com>
2025-09-02 01:02:39 +0600

    Add files via upload

---
+++ Six.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Six{
+    public static void main(String[] args){
+        System.out.println("enter a number");
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
