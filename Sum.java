a2ebabc6e7d6a489b9c92c52c9580cbad1ac8062
ariful599 <ariful.islam.siam599@gmail.com>
2025-09-02 10:41:36 +0600

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number N to find the sum from 1 to N: ");
+        int N = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 1 to " + N + " is: " + sum);
+
+        scanner.close();
+    }
+}
