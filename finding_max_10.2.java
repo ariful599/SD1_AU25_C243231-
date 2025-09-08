e68ddbba7f6c59f07d952cea9a7fba810cabeabd
ariful599 <ariful.islam.siam599@gmail.com>
2025-09-08 19:58:54 +0600

    Add files via upload

---
+++ finding_max_10.2.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+class {
+    public static void finding_max_10.2(String[] args) {
+        System.out.println("enter the array element");
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
