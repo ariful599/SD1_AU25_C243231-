ea8cf0a3331c2cac3e1e888017aec750df64f131
ariful599 <ariful.islam.siam599@gmail.com>
2025-09-09 10:43:11 +0600

    Add files via upload

---
+++ Subscription.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Subscription {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+         System.out.println("enter the number of student");
+         int x=sc.nextInt();
+         System.out.println("enter the total amount of money for one subscription");
+         int y=sc.nextInt();
+         int min_money= y/x;
+         System.out.println(min_money);
+         
+         
+    }
+}
+       
