ef01fd2fc2b26192ddd6cf02fd9c122cc37b1c08
ariful599 <ariful.islam.siam599@gmail.com>
2025-09-17 22:09:01 +0600

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Assingment{
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter startig time");
+        int x= sc.nextInt();
+       if(x*3>10){
+               System.out.println("he can finish on time");
+       }
+       else{
+               System.out.println("he can not finish on time");
+       }
+        
+        
+    }
+}
